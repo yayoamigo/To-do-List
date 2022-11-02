868cf4d80f3f538b9242453bac7d7e7770acdf18
David Zambrano <david._101@hotmail.com>
2022-11-02 17:06:55 -0500

    Add linters

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import './styles.css';
 
-const body = document.getElementById('item-list')
+const body = document.getElementById('item-list');
 
 const arrayObj = [
- {description: "Feed my dog",
- completed: false,
- index: 0,
- },
- {
-  description: "Feed my cat",
- completed: false,
- index: 1,
- },
- {
-  description: "Feed my frog",
- completed: false,
- index: 2,
- },
-]
+  {
+    description: 'Feed my dog',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Feed my cat',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Feed my frog',
+    completed: false,
+    index: 2,
+  },
+];
 
 const fillList = () => {
- arrayObj.forEach((x)=>{
- let content = `<li>
+  arrayObj.forEach((x) => {
+    const content = `<li>
  <button class="${x.completed}"> 
   <i class="fa-regular fa-square"></i>
  </button>
@@ -30,9 +31,8 @@ const fillList = () => {
  </div>
  <button class="${x.index}"> <i class="fa-solid fa-ellipsis-vertical"></i></button>
 </li>`;
- body.insertAdjacentHTML("beforeend", content);
-})
-}
+    body.insertAdjacentHTML('beforeend', content);
+  });
+};
 
 window.addEventListener('DOMContentLoaded', fillList);
-
--- webpack.config.js
+++ webpack.config.js
@@ -2,31 +2,31 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
- mode: 'development',
- entry: path.resolve(__dirname, 'src/index.js'),
- output: {
-  path: path.resolve(__dirname, 'dist'),
-  filename: 'bundle.js',
- },
- devServer: {
-  static: {
-   directory: path.resolve(__dirname, 'dist'),
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
-  port: 3000,
-  open: true,
-  hot: true,
- },
- plugins: [
-  new HtmlWebpackPlugin({
-    template: './src/template.html',
-  }),
-],
- module: {
-  rules: [
-    {
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
     },
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
   ],
-},
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
