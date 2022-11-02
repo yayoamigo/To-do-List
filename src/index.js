a8ddad3bffb86e9b6becaf6368bef39ee7704b26
David Zambrano <david._101@hotmail.com>
2022-11-02 17:02:34 -0500

    Add loaders and webdev server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log("hello world");
+import './styles.css';
+
+const body = document.getElementById('item-list')
+
+const arrayObj = [
+ {description: "Feed my dog",
+ completed: false,
+ index: 0,
+ },
+ {
+  description: "Feed my cat",
+ completed: false,
+ index: 1,
+ },
+ {
+  description: "Feed my frog",
+ completed: false,
+ index: 2,
+ },
+]
+
+const fillList = () => {
+ arrayObj.forEach((x)=>{
+ let content = `<li>
+ <button class="${x.completed}"> 
+  <i class="fa-regular fa-square"></i>
+ </button>
+ <div class="view">
+  <label class="label">${x.description}</label>
+ </div>
+ <button class="${x.index}"> <i class="fa-solid fa-ellipsis-vertical"></i></button>
+</li>`;
+ body.insertAdjacentHTML("beforeend", content);
+})
+}
+
+window.addEventListener('DOMContentLoaded', fillList);
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
  mode: 'development',
@@ -7,4 +8,25 @@ module.exports = {
   path: path.resolve(__dirname, 'dist'),
   filename: 'bundle.js',
  },
-}
+ devServer: {
+  static: {
+   directory: path.resolve(__dirname, 'dist'),
+  },
+  port: 3000,
+  open: true,
+  hot: true,
+ },
+ plugins: [
+  new HtmlWebpackPlugin({
+    template: './src/template.html',
+  }),
+],
+ module: {
+  rules: [
+    {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+  ],
+},
+};
