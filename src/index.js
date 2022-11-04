b76c9ad896cffbd7fc1b3e1ec150ca06cab198fc
David Zambrano <david._101@hotmail.com>
2022-11-03 21:46:32 -0500

    Implement edit button functionality

--- src/index.js
+++ src/index.js
@@ -3,57 +3,57 @@ import './styles.css';
 const body = document.getElementById('item-list');
 const form = document.getElementById('new-task-form');
 
-//First we need a task class to create the array for our new tasks and the methods to add and remove those tasks to and from the 
-//local storage
-class tasks {
-
+// First we need a task class to create the array for our new tasks and the methods to add and
+// remove those tasks to and from the
+// local storage
+class Tasks {
   static taskObj = [];
 
-  constructor(description){
+  constructor(description) {
     this.description = description;
     this.completed = false;
-    this.index = tasks.taskObj.length + 1;
+    this.index = Tasks.taskObj.length + 1;
   }
 
-  static taskPush(newTask){
-    tasks.taskObj.push(newTask);
+  static taskPush(newTask) {
+    Tasks.taskObj.push(newTask);
   }
 
-  static removeTask(element){
+  static removeTask(element) {
     element.parentElement.remove();
   }
-
 }
 
-//When the user submits a new task we create a new instance of the task obj, and call the methods to add that task to local storage
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const taskInput = document.getElementById('new-task-input');
-  const newTask = new tasks(taskInput.value);
-  fillList(newTask);
-  tasks.taskPush(newTask);
-  localStorage.setItem('taskArray', JSON.stringify(tasks.taskObj));
-  taskInput.value = '';
-});
-
-//This function gets call and insert the new task into the HTML
+// This function gets call and insert the new task into the HTML
 const fillList = (newTask) => {
   const content = `<li>
   <button class="${newTask.completed}"> 
   <i class="fa-regular fa-square"></i>
   </button>
   <div class="view">
-  <label class="label" id="description-input">${newTask.description}</label>
+  <input type="text" class="description-input" value="${newTask.description}" readonly="">
   </div>
-  <button class="remove-btn"> remove</button>
-  <button class="${newTask.index}"> <i class="fa-solid fa-ellipsis-vertical"></i></button>
+  <button class="edit" id="${newTask.description}"> edit</button>
+  <button class="remove"> remove</button>
   </li>`;
   body.insertAdjacentHTML('beforeend', content);
 };
 
-//Function for emoving the task from the local storage
+// When the user submits a new task we create a new instance of the task obj, and call the methods
+// to add that task to local storage
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const taskInput = document.getElementById('new-task-input');
+  const newTask = new Tasks(taskInput.value);
+  fillList(newTask);
+  Tasks.taskPush(newTask);
+  localStorage.setItem('taskArray', JSON.stringify(Tasks.taskObj));
+  taskInput.value = '';
+});
+
+// Function for emoving the task from the local storage
 const removeTasks = (task) => {
-  const taskDesc = task.querySelector('#description-input').innerText;
+  const taskDesc = task.querySelector('input').value;
   const x = JSON.parse(localStorage.getItem('taskArray'));
   const filterIndex = x.findIndex((x) => x.description === taskDesc);
   x.splice(filterIndex, 1);
@@ -63,14 +63,30 @@ const removeTasks = (task) => {
   localStorage.setItem('taskArray', JSON.stringify(x));
 };
 
-//Even for actually removing the task from the UI and the local storage when the remove btn gets clicked
 body.addEventListener('click', (e) => {
   const task = e.target.parentElement;
-  removeTasks(task);
-  tasks.removeTask(e.target);
+  const id = e.target.getAttribute('id');
+  if (e.target.classList.contains('remove')) {
+    removeTasks(task);
+    Tasks.removeTask(e.target);
+  }
+  // task will be edited when first the input field of task is updated and then edit icon is clicked
+  if (e.target.classList.contains('edit')) {
+    const content = document.querySelector('.view');
+    const input = content.querySelector('input');
+    input.removeAttribute('readonly');
+    input.focus();
+    input.addEventListener('blur', (e) => {
+      input.setAttribute('readonly', true);
+      const x = JSON.parse(localStorage.getItem('taskArray'));
+      const filterIndex = x.findIndex((x) => x.description === id);
+      x[filterIndex].description = e.target.value;
+      localStorage.setItem('taskArray', JSON.stringify(x));
+    });
+  }
 });
 
-//Function for displaying the taks from the local storage
+// Function for displaying the taks from the local storage
 const displayTasks = () => {
   const tasks = JSON.parse(localStorage.getItem('taskArray'));
   tasks.forEach((x) => fillList(x));
